5950850ca9184244b002446f10f5b1e97247742d
Onabanji <rajimoshood200482@gmail.com>
2025-06-18 00:29:54 +0100

    initial commit

---
+++ components/project-overview.tsx
@@ -0,0 +1,33 @@
+import NextLink from "next/link";
+export const ProjectOverview = () => {
+  return (
+    <div className="flex flex-col items-center justify-end">
+      <h1 className="text-3xl font-semibold mb-4">RJB World's Third Eye</h1>
+      <p className="text-center">
+        Welcome to the Gate of Dreams. Your subconscious is speaking. This is
+        your sanctuary to record, reflect, and receive meaning from the hidden
+        realms. Begin by sharing your most recent dream. No matter how strange,
+        confusing, or fragmented — everything carries light. 🪶 Let’s begin the
+        decoding.
+      </p>
+    </div>
+  );
+};
+
+const Link = ({
+  children,
+  href,
+}: {
+  children: React.ReactNode;
+  href: string;
+}) => {
+  return (
+    <NextLink
+      target="_blank"
+      className="text-blue-500 hover:text-blue-600 transition-colors duration-75"
+      href={href}
+    >
+      {children}
+    </NextLink>
+  );
+};
