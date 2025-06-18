a781d1752931f4f1fb91b9d59072862284d3e25e
Onabanji <rajimoshood200482@gmail.com>
2025-06-18 01:24:57 +0100

    removed apostrophe

--- components/project-overview.tsx
+++ components/project-overview.tsx
@@ -1,7 +1,7 @@
 export const ProjectOverview = () => {
   return (
     <div className="flex flex-col items-center justify-end">
-      <h1 className="text-3xl font-semibold mb-4">RJB World's Third Eye</h1>
+      <h1 className="text-3xl font-semibold mb-4">RJB World Third Eye</h1>
       <p className="text-center">
         Welcome to the Gate of Dreams. Your subconscious is speaking. This is
         your sanctuary to record, reflect, and receive meaning from the hidden
